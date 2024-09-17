031fc86ba79dc9af06508fec826c0c9e41a357b1
Pratap <devendra.pratap@hostbooks.com>
2024-09-17 23:46:32 +0530

    backend-commit

---
+++ src/main/java/com/dev/myselflog/MyselflogCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.dev.myselflog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselflogCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyselflogCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/myselflog/MyselflogCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.myselflog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselflogCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
