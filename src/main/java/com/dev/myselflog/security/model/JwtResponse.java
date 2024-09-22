06fb19fdb41872fd945f7068ead05a28ebc9e65c
Pratap <pratapdevendra307@gmail.com>
2024-09-23 00:07:46 +0530

    :tada: Security with JWT

---
+++ src/main/java/com/dev/myselflog/security/model/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.dev.myselflog.security.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtResponse {
+    private String jwtToken;
+    private String username;
+}
